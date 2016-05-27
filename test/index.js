ba94cf787807e985975042eb3b7ca673080b6a45
Michael Tuttle <openam@gmail.com>
2016-05-26 22:04:41 -0600

    test: remove lodash from testing

--- test/index.js
+++ test/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-/* eslint no-unused-expressions:0 */
-
-const _         = require( 'lodash' );
 const lapinMock = require( 'lapin-mock' );
 
 require( 'should' );
@@ -45,110 +42,114 @@ describe( '@sinet/lapin-send-error', function () {
 		} );
 	} );
 
-	describe( '-- handle JSend error', function () {
-		const baseError = {
-			'status' : 'error'
-		};
+	describe( '-- error with data', function () {
+		before( function ( done ) {
+			error = {
+				'status'  : 'error',
+				'message' : 'Some error',
+				'data'    : 'Some data'
+			};
 
-		describe( '-- error with data', function () {
-			before( function ( done ) {
-				error = _.defaults( {
-					'message' : 'Some error',
-					'data'    : 'Some data'
-				}, baseError );
+			sendMock = new lapinMock.SendMock( done );
 
-				sendMock = new lapinMock.SendMock( done );
-
-				handleError( sendMock, error );
-			} );
-
-			it( 'should not duplicate data', function () {
-				sendMock.should.have.property( 'errorMessage' ).and.equal( 'Some error' );
-				sendMock.should.have.property( 'error' ).and.equal( error.data );
-				sendMock.should.have.property( 'code' ).and.equal( undefined );
-			} );
+			handleError( sendMock, error );
 		} );
 
-		describe( '-- error with no codes', function () {
-			before( function ( done ) {
-				error    = _.defaults( { 'message' : 'Some error' }, baseError );
-				sendMock = new lapinMock.SendMock( done );
+		it( 'should not duplicate data', function () {
+			sendMock.should.have.property( 'errorMessage' ).and.equal( 'Some error' );
+			sendMock.should.have.property( 'error' ).and.equal( error.data );
+			sendMock.should.have.property( 'code' ).and.equal( undefined );
+		} );
+	} );
 
-				handleError( sendMock, error );
-			} );
+	describe( '-- error with no codes', function () {
+		before( function ( done ) {
+			error = {
+				'status'  : 'error',
+				'message' : 'Some error'
+			};
 
-			it( 'should not have a code defined', function () {
-				sendMock.should.have.property( 'errorMessage' ).and.equal( 'Some error' );
-				sendMock.should.have.property( 'error' ).and.equal( error );
-				sendMock.should.have.property( 'code' ).and.equal( undefined );
-			} );
+			sendMock = new lapinMock.SendMock( done );
+
+			handleError( sendMock, error );
 		} );
 
-		describe( '-- error with new code', function () {
-			before( function ( done ) {
-				error    = _.defaults( { 'message' : 'Error with code' }, baseError );
-				sendMock = new lapinMock.SendMock( done );
+		it( 'should not have a code defined', function () {
+			sendMock.should.have.property( 'errorMessage' ).and.equal( 'Some error' );
+			sendMock.should.have.property( 'error' ).and.equal( error );
+			sendMock.should.have.property( 'code' ).and.equal( undefined );
+		} );
+	} );
 
-				handleError( sendMock, error, 'code' );
-			} );
+	describe( '-- error with new code', function () {
+		before( function ( done ) {
+			error = {
+				'status'  : 'error',
+				'message' : 'Error with code'
+			};
+			sendMock = new lapinMock.SendMock( done );
 
-			it( 'should have new error code', function () {
-				sendMock.should.have.property( 'errorMessage' ).and.equal( 'Error with code' );
-				sendMock.should.have.property( 'error' ).and.equal( error );
-				sendMock.should.have.property( 'code' ).and.equal( 'code' );
-			} );
+			handleError( sendMock, error, 'code' );
 		} );
 
-		describe( '-- error with existing code', function () {
-			before( function ( done ) {
-				error = _.defaults( {
-					'message' : 'Error with existing code',
-					'code'    : 'existing'
-				}, baseError );
+		it( 'should have new error code', function () {
+			sendMock.should.have.property( 'errorMessage' ).and.equal( 'Error with code' );
+			sendMock.should.have.property( 'error' ).and.equal( error );
+			sendMock.should.have.property( 'code' ).and.equal( 'code' );
+		} );
+	} );
 
-				sendMock = new lapinMock.SendMock( done );
+	describe( '-- error with existing code', function () {
+		before( function ( done ) {
+			error = {
+				'status'  : 'error',
+				'message' : 'Error with existing code',
+				'code'    : 'existing'
+			};
 
-				handleError( sendMock, error );
-			} );
+			sendMock = new lapinMock.SendMock( done );
 
-			it( 'should have existing error code', function () {
-				sendMock.should.have.property( 'errorMessage' ).and.equal( 'Error with existing code' );
-				sendMock.should.have.property( 'error' ).and.equal( error );
-				sendMock.should.have.property( 'code' ).and.equal( 'existing' );
-			} );
+			handleError( sendMock, error );
 		} );
 
-		describe( '-- error with new and existing code', function () {
-			before( function ( done ) {
-				error = _.defaults( {
-					'message' : 'Error with existing code',
-					'code'    : 'existing'
-				}, baseError );
+		it( 'should have existing error code', function () {
+			sendMock.should.have.property( 'errorMessage' ).and.equal( 'Error with existing code' );
+			sendMock.should.have.property( 'error' ).and.equal( error );
+			sendMock.should.have.property( 'code' ).and.equal( 'existing' );
+		} );
+	} );
 
-				sendMock = new lapinMock.SendMock( done );
+	describe( '-- error with new and existing code', function () {
+		before( function ( done ) {
+			error = {
+				'status'  : 'error',
+				'message' : 'Error with existing code',
+				'code'    : 'existing'
+			};
 
-				handleError( sendMock, error, 'new' );
-			} );
+			sendMock = new lapinMock.SendMock( done );
 
-			it( 'should have `existing,new` error code', function () {
-				sendMock.should.have.property( 'errorMessage' ).and.equal( 'Error with existing code' );
-				sendMock.should.have.property( 'error' ).and.equal( error );
-				sendMock.should.have.property( 'code' ).and.equal( 'existing,new' );
-			} );
+			handleError( sendMock, error, 'new' );
 		} );
 
-		describe( '-- error that is just a string', function () {
-			before( function ( done ) {
-				error    = 'Error with existing code';
-				sendMock = new lapinMock.SendMock( done );
+		it( 'should have `existing,new` error code', function () {
+			sendMock.should.have.property( 'errorMessage' ).and.equal( 'Error with existing code' );
+			sendMock.should.have.property( 'error' ).and.equal( error );
+			sendMock.should.have.property( 'code' ).and.equal( 'existing,new' );
+		} );
+	} );
 
-				handleError( sendMock, error );
-			} );
+	describe( '-- error that is just a string', function () {
+		before( function ( done ) {
+			error    = 'Error with existing code';
+			sendMock = new lapinMock.SendMock( done );
 
-			it( 'should have have the error be the message and data', function () {
-				sendMock.should.have.property( 'errorMessage' ).and.equal( error );
-				sendMock.should.have.property( 'error' ).and.equal( error );
-			} );
+			handleError( sendMock, error );
+		} );
+
+		it( 'should have have the error be the message and data', function () {
+			sendMock.should.have.property( 'errorMessage' ).and.equal( error );
+			sendMock.should.have.property( 'error' ).and.equal( error );
 		} );
 	} );
 } );
